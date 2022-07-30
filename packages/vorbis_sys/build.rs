9b3da5b9d9227b7547d078d637cfa2cb48a94e72
Alejandro González <AlexTMjugador@users.noreply.github.com>
2022-07-30 22:32:55 +0200

    fix(workflows/ci): do not run type layout tests on Windows
    They assume an Unix-like platform, but Windows is far from being
    anything like that.

--- packages/vorbis_sys/build.rs
+++ packages/vorbis_sys/build.rs
@@ -94,6 +94,8 @@ fn main() {
 		.include(env::var_os("DEP_OGG_INCLUDE").unwrap())
 		.include(vorbis_vendor_path.join("lib"))
 		.include(include_path)
+		// Fixes building on Windows with the GCC toolchain, and maybe other
+		// platforms with a modern C compiler
 		.define("HAVE_STDINT_H", None)
 		.compile("vorbis");
 }
