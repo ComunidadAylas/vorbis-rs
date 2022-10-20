aedab43672a4c475357bcedd2fd503cd4a3f4451
Alejandro González <AlexTMjugador@users.noreply.github.com>
2022-10-20 17:42:15 +0200

    chore: tweak some comments

--- packages/vorbis/src/common.rs
+++ packages/vorbis/src/common.rs
@@ -11,7 +11,19 @@ mod vorbis;
 // self:: must be used in order to clear up ambiguity
 pub use self::vorbis::{VorbisComments, VorbisInfo};
 
-// taken from stdlib's assume_init implementation for Box
+/// Converts to `Box<T>`.
+///
+/// # Safety
+/// As with [`MaybeUninit::assume_init`], it is up to the caller to guarantee that the value
+/// really is in an initialized state. Calling this when the content is not yet fully
+/// initialized causes immediate undefined behavior.
+///
+/// [`MaybeUninit::assume_init`]: mem::MaybeUninit::assume_init
+///
+/// # Highlights
+/// This function is a backport of the unstable `Box::assume_init` method to stable Rust,
+/// tracked by [issue 63291](https://github.com/rust-lang/rust/issues/63291).
+// FIXME refactor this out once Box::assume_init is made stable, if ever
 pub unsafe fn assume_init_box<T>(boxed: Box<MaybeUninit<T>>) -> Box<T> {
 	Box::from_raw(Box::into_raw(boxed) as *mut T)
 }
