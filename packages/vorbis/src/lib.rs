bcbf06ec5b36c0579d3de861ba5b205563e18b44
Alejandro González <AlexTMjugador@users.noreply.github.com>
2022-10-21 15:11:54 +0200

    chore: improve crate docs. Move transcode test to rustdoc example
    This rustdoc example is also considered a test when running cargo test,
    so it can be used for both testing and documentation purposes.

--- packages/vorbis/src/lib.rs
+++ packages/vorbis/src/lib.rs
@@ -8,9 +8,60 @@
 //!
 //! [aoTuV]: https://github.com/AO-Yumi/vorbis_aotuv
 //! [Lancer]: https://web.archive.org/web/20160408055651/http://homepage3.nifty.com/blacksword/index.htm
-
-#[cfg(test)]
-mod tests;
+//!
+//! # Known limitations
+//!
+//! Chained Ogg Vorbis streams (i.e., with several consecutive logical bitstreams) are not supported for
+//! now. These are seldom found, however. Feel free to file an issue or open a pull request if you are
+//! interested in chained stream support.
+//!
+//! # Examples
+//!
+//! The following example transcodes an Ogg Vorbis file to another in-memory Ogg Vorbis stream,
+//! showing how the API offered by this crate works together to achieve this task.
+//!
+//! ```
+//! # fn main() -> Result<(), Box<dyn std::error::Error>> {
+//! # use std::fs::File;
+//! # use std::io::Cursor;
+//! # use vorbis::{VorbisBitrateManagementStrategy, VorbisDecoder, VorbisEncoder};
+//! #
+//! # let mut source_ogg = &include_bytes!("../../vorbis_sys/src/8khz_500ms_mono_400hz_sine_wave.ogg")[..];
+//! # #[cfg(__example_only)]
+//! let mut source_ogg = File::open("audio.ogg")?;
+//! let mut transcoded_ogg = vec![];
+//!
+//! let mut decoder = VorbisDecoder::new(&mut source_ogg)?;
+//! let mut encoder = VorbisEncoder::new(
+//!     0, // Stream serial. Please consider generating a random one for production use
+//!     [("", ""); 0], // No comments
+//!     decoder.sampling_frequency(),
+//!     decoder.channels(),
+//!     VorbisBitrateManagementStrategy::QualityVbr {
+//!         // This is a very low quality factor, meant to generate the smallest files and
+//!         // showcase aoTuV features. You maybe not want to use it
+//!         target_quality: -0.2
+//!     },
+//!     None, // Let the libraries choose a minimum Ogg page size
+//!     &mut transcoded_ogg
+//! )?;
+//!
+//! while let Some(decoded_block) = decoder.decode_audio_block()? {
+//!     encoder.encode_audio_block(decoded_block.samples())?;
+//! }
+//!
+//! // The encoder is automatically finished on drop, so calling finish explicitly is
+//! // only needed if you want to handle any errors
+//! encoder.finish()?;
+//!
+//! // Do whatever you want with transcoded_ogg now
+//!
+//! # // Decode the generated output as a sanity check
+//! # let mut decoder = VorbisDecoder::new(Cursor::new(transcoded_ogg))?;
+//! # while decoder.decode_audio_block()?.is_some() {}
+//! # Ok(())
+//! # }
+//! ```
 
 /// A constant whose numeric value represents a libvorbis `OV_EBADHEADER` error, which may be
 /// returned as a [`LibraryError`](VorbisError::LibraryError) error code.
